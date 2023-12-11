9445427e433b372943c462165d62d02d17960c85
thienautran <thien.autran75@gmail.com>
2023-12-12 04:24:50 +1100

    feat: adds new chess board implimentation

---
+++ app/(projects)/chess/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ChessLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
+}
--- app/(projects)/chess/page.tsx
+++ app/(projects)/chess/page.tsx
@@ -1,7 +1,47 @@
 export default function ChessPage() {
+  function generateLists() {
+    const numOfLists = 8;
+    let lists = [];
+
+    for (let i = 0; i < numOfLists; i++) {
+      let cells = [];
+      for (let j = 0; j < 8; j++) {
+        cells.push(0);
+      }
+      lists.push(cells);
+    }
+
+    return lists;
+  }
+
+  const testBoard = generateLists();
+
   return (
-    <div>
-      <h1>Froggy Project - Chess</h1>
+    <div className='bg-slate-950 min-h-screen flex flex-col justify-center items-center text-white'>
+      <h1 className='text-3xl text-center'>Froggy Project - Chess</h1>
+      <div
+        role='grid'
+        className='grid grid-rows-8 h-[600px] w-[600px]'
+      >
+        {testBoard.map((row, i) => (
+          <div
+            role='row'
+            className='grid grid-cols-8'
+          >
+            {row.map((cols, j) => (
+              <div
+                role='gridcell'
+                className={`h-full w-full ${
+                  (i + j) % 2 ? 'bg-yellow-900' : 'bg-yellow-50'
+                }`}
+              >
+                {i}
+                {j}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
