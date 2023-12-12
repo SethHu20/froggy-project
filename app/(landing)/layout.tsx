ddaedb9eaab69cddf6dfcb0979087b3ace3a1b56
Hsuen-Hao Hou <hhou0007@student.monash.edu>
2023-12-13 03:57:53 +1100

    Updated Chess project UI to reflect landing page theme

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import logo from "@/public/at-glass-logo.png";
 import NavBar from "@/components/NavBar";
+import Logo from "@/components/Logo";
 
 
 
@@ -15,21 +16,7 @@ export default function LandingLayout({
     <div className="bg-slate-800 text-white flex flex-col items-center h-screen min-h-screen w-full min-w-fit">
       <div className=" flex flex-col items-center max-w-screen-lg w-full px-10">
         <header className="flex flex-row justify-between items-center w-full flex-wrap gap-4 pt-5 pb-10">
-          <Link
-            href="/"
-            className="group flex flex-row items-center text-xl space-x-3 text-gray-100 w-fit hover:bg-blue-100 hover:text-slate-900 p-3 rounded-full transition-all"
-          >
-            <Image
-              src={logo}
-              alt="Froggy Project Logo"
-              width={50}
-              height={50}
-              className="drop-shadow-3xl group-hover:animate-standingBounce"
-            />
-            <span className="pr-3">
-              <strong>Froggy Project</strong>
-            </span>
-          </Link>
+          <Logo />
           <NavBar />
         </header>
         {children}
--- app/(projects)/chess/page.tsx
+++ app/(projects)/chess/page.tsx
@@ -1,49 +1,16 @@
+import Chessboard from "@/chess/ui/Chessboard";
+import Logo from "@/components/Logo";
+import { FaChevronRight } from "react-icons/fa";
+
 export default function ChessPage() {
-  function generateLists() {
-    const numOfLists = 8;
-    let lists = [];
-
-    for (let i = 0; i < numOfLists; i++) {
-      let cells = [];
-      for (let j = 0; j < 8; j++) {
-        cells.push(0);
-      }
-      lists.push(cells);
-    }
-
-    return lists;
-  }
-
-  const testBoard = generateLists();
-
   return (
-    <div className='bg-slate-950 min-h-screen flex flex-col justify-center items-center text-white'>
-      <h1 className='text-3xl text-center'>Froggy Project - Chess</h1>
-      <div
-        role='grid'
-        className='grid grid-rows-8 h-[600px] w-[600px]'
-      >
-        {testBoard.map((row, i) => (
-          <div
-            key={i}
-            role='row'
-            className='grid grid-cols-8'
-          >
-            {row.map((_, j) => (
-              <div
-                key={i << 3 + j}
-                role='gridcell'
-                className={`h-full w-full ${
-                  (i + j) % 2 ? 'bg-yellow-900' : 'bg-yellow-50'
-                }`}
-              >
-                {i}
-                {j}
-              </div>
-            ))}
-          </div>
-        ))}
+    <div className="bg-slate-800 text-white flex flex-col items-center h-screen min-h-screen w-full min-w-fit">
+      <div className="flex flex-row items-center w-full p-5">
+        <Logo />
+        <FaChevronRight className="text-xl" />
+        <h1 className="ml-4 text-2xl">Chess</h1>
       </div>
+      <Chessboard showCoord={true} />
     </div>
   );
 }
