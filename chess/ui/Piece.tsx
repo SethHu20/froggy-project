d137e6db39cd1f664478b6ac40f1439e45ec6664
Seth Hu <sethhu20@gmail.com>
2023-12-15 17:09:10 +1100

    Chessboard and pieces are rescalable and moves together

--- chess/ui/Piece.tsx
+++ chess/ui/Piece.tsx
@@ -2,20 +2,37 @@
 
 import Image from "next/image";
 import { PieceType, get_svg } from "./PieceType";
+import { Rnd } from "react-rnd";
 
 export default function Piece({
   piece,
-  coord,
+  size,
+  origin,
+  updatePieceLocation
 }: {
   piece: PieceType;
-  coord: { x: number; y: number };
+  size: number;
+  origin: { x: number; y: number };
+  updatePieceLocation: (x: number, y: number) => void;
 }) {
   return (
-    <Image
+    <Rnd
+      size={{ width: size, height: size }}
+      position={origin}
+      onDragStop={(e, d) => {
+        updatePieceLocation(d.x, d.y);
+      }}
+      bounds="parent"
+      enableResizing={false}
+      enableUserSelectHack={true}
+    >
+      <Image
         alt={`${piece.player} ${piece.piece}`}
         src={get_svg(piece)}
-        width={50}
-        height={50}
+        fill
+        sizes="100%"
+        className="pointer-events-none"
       />
+    </Rnd>
   );
 }
