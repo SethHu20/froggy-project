f40956a63bc33548dc991c2dd0d8104c198777f5
Seth Hu <sethhu20@gmail.com>
2024-04-11 09:03:34 +1000

    Fix background layout color in homepage

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -13,8 +13,8 @@ export default function LandingLayout({
 }) {
 
   return (
-    <div className="bg-slate-800 text-white flex flex-col items-center h-screen min-h-screen w-full min-w-fit">
-      <div className=" flex flex-col items-center max-w-screen-lg w-full px-10">
+    <div className="bg-slate-800 text-white flex flex-col items-center">
+      <div className=" flex flex-col items-center max-w-screen-lg w-full px-10 pb-10">
         <header className="flex flex-row justify-between items-center w-full flex-wrap gap-4 pt-5 pb-10">
           <Logo />
           <NavBar />
